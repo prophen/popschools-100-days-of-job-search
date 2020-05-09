919329c967fc0aea770c5ea85805e7b2375f6c09
n.p <3941856+prophen@users.noreply.github.com>
2020-05-08 18:32:39 -0700

    💄 add links to navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,7 +3,10 @@ import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
-  <nav className="navigation"> 
+  <nav className="navigation">
+    <a href="https://popschools.club">PopSchools Community</a> 
+    <a href="https://twitter.com/100dojs">@100dojs on Twitter</a> 
+    <a href="https://remind.com/join/100dojs">Get text reminders</a> 
     <ThemeChanger/>
   </nav>
   
