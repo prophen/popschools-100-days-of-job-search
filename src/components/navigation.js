fee946714f708925ac9448c704ceb61c2202c034
n.p <3941856+prophen@users.noreply.github.com>
2020-05-12 11:10:25 -0700

    💬 Update header links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,8 +4,8 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation">
-    <a href="https://popschools.club">PopSchools Community</a> 
-    <a href="https://twitter.com/100dojs">@100dojs on Twitter</a> 
+    <a href="https://popschools.club">Community</a> 
+    <a href="https://twitter.com/100dojs">@100dojs</a> 
     <a href="https://remind.com/join/100dojs">Get text reminders</a> 
     <ThemeChanger/>
   </nav>
